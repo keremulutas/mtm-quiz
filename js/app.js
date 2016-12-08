6e28ea66451376c4922bfcc8f7caa82142439faf
Kerem Ulutaş <kerem@ulutas.gen.tr>
2016-12-08 23:10:40 +0300

    added drilldown style for menus with children

--- js/app.js
+++ js/app.js
@@ -18,31 +18,40 @@ mtmApp.controller("NewsListController", ["$rootScope", "$scope", "$http", functi
 mtmApp.directive("mtmMenu", function() {
     return {
         replace: true,
-        template: '<ul class="nav nav-pills nav-stacked"><li ng-repeat="t in tree" ng-if="t.label"><a href="#" data-source-id="{{t.id}}" ng-click="go($event)">{{t.label}}</a><div style="padding-left: 20px;"><mtm-menu tree="t.children"></mtm-menu></div></li></ul>',
+        template: '<ul class="nav nav-pills nav-stacked {{ isCollapsible ? \'collapse\' : \'\' }}"><li ng-repeat="t in tree" ng-if="t.label"><a href="#" data-toggle="collapse" data-target="#drilldown-{{t.id}}" data-source-id="{{t.id}}" ng-click="go($event)">{{t.label}}</a><div style="padding-left: 20px;"><mtm-menu id="drilldown-{{t.id}}" is-collapsible="true" tree="t.children"></mtm-menu></div></li></ul>',
         scope: {
             tree: "=",
+            isCollapsible: "=",
         },
         controller: function($rootScope, $scope) {
             $scope.go = function($evt) {
-                $evt.preventDefault();
-                $evt.stopPropagation();
+                // $evt.preventDefault();
+                // $evt.stopPropagation();
                 var elem = $($evt.target);
-                if ($("#menuWrapper").find("li.active").is(elem)) {
+                if (elem.hasClass("active")) {
                     console.warn("same link clicked.");
                     return;
                 }
-                $("#menuWrapper").find("li.active").removeClass("active");
+                $("#menuWrapper").find("li a.active").removeClass("active");
                 elem.addClass("active");
+
                 var menuId = elem.data("sourceId");
-                $rootScope.currentDocs = [];
+                var filteredDocs = [];
                 $rootScope.docs.forEach(function(doc, idx) {
                     if (doc.brands[0].id === menuId) {
-                        $rootScope.currentDocs.push(doc);
+                        filteredDocs.push(doc);
                     }
                 });
-                $("html, body").animate({
-                    scrollTop: 0,
-                }, "fast");
+
+                if(filteredDocs.length) {
+                    $rootScope.currentDocs = filteredDocs;
+                    $("html, body").animate({
+                        scrollTop: 0,
+                    }, "fast");
+                }
+
+                $evt.preventDefault();
+                return false;
             };
         },
     };
